3c2c95e97efb46f91e296702e9dccb6b12a0336e
houlinwei <houlinwei@xiaomi.com>
2017-03-24 15:55:18 +0800

    err

--- src/err/main.go
+++ src/err/main.go
@@ -1,1 +1,30 @@
-package err
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	Code int
+	Msg  string
+}
+
+func (e MyError) Error() string {
+	e.When = time.Now()
+	return fmt.Sprintf("At: %s, Code:%d, Msg:%s.", e.When, e.Code, e.Msg)
+}
+
+func Cause(c int, s string) error {
+	return MyError{
+		Code: c,
+		Msg:  s,
+	}
+}
+
+func main() {
+	if err := Cause(-1, "Maybe Something Wrong."); err != nil {
+		fmt.Println(err)
+	}
+}
