f5ea501c10eb3b683366b03136ebe086c40660ac
houlinwei <houlinwei@xiaomi.com>
2017-03-28 19:45:00 +0800

    sync

--- src/kafka_client/main.go
+++ src/kafka_client/main.go
@@ -1,1 +1,34 @@
-package kafka_client
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"log"
+)
+
+func Produce() {
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	msg := &sarama.ProducerMessage{Topic: "houlinwei.test", Value: sarama.StringEncoder("toda!")}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("FAILED to send message: %s\n", err)
+	} else {
+		log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+	}
+}
+
+func Consume() {
+
+}
+
+func main() {
+	Produce()
+}
