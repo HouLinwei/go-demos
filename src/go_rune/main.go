2702e1678fa1d5a168a14ec1325288d34b22de17
houlinwei <houlinwei@xiaomi.com>
2017-03-30 19:20:32 +0800

    rune

---
+++ src/go_rune/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main()  {
+	s := "123abc!!!你好！"
+	for _, v := range s{
+		fmt.Printf("%c \t",v)
+	}
+	fmt.Println()
+	for _, v := range s{
+		fmt.Printf("%d \t",v)
+	}
+	fmt.Println()
+	fmt.Println("==========")
+
+	b :=[]byte(s)
+	for _, v := range b{
+		fmt.Printf("%c \t",v)
+	}
+	fmt.Println()
+	for _, v := range b{
+		fmt.Printf("%d \t",v)
+	}
+	fmt.Println()
+	fmt.Println("==========")
+
+	r :=[]rune(s)
+	for _, v := range r{
+		fmt.Printf("%c \t",v)
+	}
+	fmt.Println()
+	for _, v := range r{
+		fmt.Printf("%d \t",v)
+	}
+	fmt.Println()
+	fmt.Println("==========")
+}
