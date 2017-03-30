32e92cdfd5df5d433645a03222ec65ce6c146b7d
houlinwei <houlinwei@xiaomi.com>
2017-03-30 19:02:56 +0800

    a simple kakfa producer and consumer

--- src/kafka_client/main.go
+++ src/kafka_client/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Shopify/sarama"
 	"log"
+	"os"
+	"os/signal"
 )
 
 func Produce() {
@@ -16,7 +19,7 @@ func Produce() {
 		}
 	}()
 
-	msg := &sarama.ProducerMessage{Topic: "houlinwei.test", Value: sarama.StringEncoder("toda!")}
+	msg := &sarama.ProducerMessage{Topic: "houlinwei.test", Value: sarama.StringEncoder("toda!123")}
 	partition, offset, err := producer.SendMessage(msg)
 	if err != nil {
 		log.Printf("FAILED to send message: %s\n", err)
@@ -26,9 +29,50 @@ func Produce() {
 }
 
 func Consume() {
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
 
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	partitionConsumer, err := consumer.ConsumePartition("houlinwei.test", 0, 2)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// Trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	consumed := 0
+ConsumerLoop:
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Println("[msg]", msg.Value)
+			log.Printf("Consumed message offset %d\n", msg.Offset)
+			consumed++
+		case <-signals:
+			break ConsumerLoop
+		}
+	}
+
+	log.Printf("Consumed: %d\n", consumed)
 }
 
 func main() {
-	Produce()
+	//Produce()
+	fmt.Println("=================")
+	Consume()
 }
