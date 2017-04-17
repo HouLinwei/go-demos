8b9f127443009dfb8ceef9c9af0730b01c55329b
houlinwei <houlinwei@xiaomi.com>
2017-04-17 12:06:11 +0800

    redis sadd

--- src/use_redis/main.go
+++ src/use_redis/main.go
@@ -1,1 +1,37 @@
-package use_redis
+package main
+
+import "fmt"
+import (
+	"gopkg.in/redis.v4"
+	"reflect"
+)
+func main()  {
+	fmt.Println("test redis command.")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	key := "test-set2"
+
+	// SADD
+	vids := []string{"int","float","uint","string",}
+	for _ , v := range vids{
+		err = client.SAdd(key, v).Err()
+		if err != nil{
+			fmt.Println(err)
+		}
+	}
+
+	// SMEMBER
+	res, err := client.SMembers(key).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(reflect.TypeOf(res))
+}
+
