411da7fd7c945ccd536c5337097a925192bcbe09
houlinwei <1507509064@qq.com>
2017-04-25 17:31:04 +0800

    kafka consumer demo

--- src/kafka_client/main.go
+++ src/kafka_client/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 )
 
 func Produce() {
@@ -19,7 +20,7 @@ func Produce() {
 		}
 	}()
 
-	msg := &sarama.ProducerMessage{Topic: "houlinwei.test", Value: sarama.StringEncoder("toda!123")}
+	msg := &sarama.ProducerMessage{Topic: "houlinwei.test2", Value: sarama.StringEncoder("toda!123")}
 	partition, offset, err := producer.SendMessage(msg)
 	if err != nil {
 		log.Printf("FAILED to send message: %s\n", err)
@@ -39,8 +40,9 @@ func Consume() {
 			log.Fatalln(err)
 		}
 	}()
-
-	partitionConsumer, err := consumer.ConsumePartition("houlinwei.test", 0, 2)
+	partitions, err:= consumer.Partitions("houlinwei.test2")
+	fmt.Println("partitions: ", partitions)
+	partitionConsumer, err := consumer.ConsumePartition("houlinwei.test2", 0, 0)
 	if err != nil {
 		panic(err)
 	}
@@ -71,8 +73,80 @@ ConsumerLoop:
 	log.Printf("Consumed: %d\n", consumed)
 }
 
+// low level consumer.
+func Consumer2(){
+	topic := "test"
+	GName := "gtest"
+	Addr := []string{"localhost:9092"}
+	config := sarama.NewConfig()
+	config.ClientID = "clien-x1"
+	client, err := sarama.NewClient(Addr, config)
+	client.Config()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	Consumer, err:= sarama.NewConsumerFromClient(client)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Get Partitions
+	ps, _ := client.Partitions(topic)
+
+	OffsetManager, err := sarama.NewOffsetManagerFromClient(GName, client)
+
+	var pcs []sarama.PartitionConsumer
+	var pcm []sarama.PartitionOffsetManager
+	for _, ps := range ps{
+		fmt.Println("partition idx: ", ps)
+		PartitionManager, _:= OffsetManager.ManagePartition(topic, ps)
+		pcm = append(pcm ,PartitionManager)
+		offset, _ := PartitionManager.NextOffset()
+		fmt.Println("offset: ", offset)
+		pConsumer, _ := Consumer.ConsumePartition(topic, ps, -2)
+		pcs = append(pcs, pConsumer)
+		//
+		of , _ := client.GetOffset(topic, ps, -1)
+		fmt.Println("Get  Off Set", of)
+	}
+
+
+	for _, ps := range ps{
+		of , _ := client.GetOffset(topic, ps, -2)
+		fmt.Println("Get  Off Set2", of)
+	}
+
+
+	var wg sync.WaitGroup
+	for idx, pc := range pcs{
+		wg.Add(1)
+		fmt.Println("yyy idx", idx, pc, pcm[idx])
+		go func(pc sarama.PartitionConsumer, pm sarama.PartitionOffsetManager) {
+			defer wg.Done()
+			// comsume it!!!
+			for {
+				select {
+				case cmsg := <-pc.Messages():
+					fmt.Println(cmsg.Value)
+					pm.MarkOffset(cmsg.Offset+1, "'")
+					//
+					of , _ := client.GetOffset(topic, 0, -1)
+					fmt.Println("Get  Off Set22220", of)
+					of1 , _ := client.GetOffset(topic, 1, -1)
+					fmt.Println("Get  Off Set22221", of1)
+				}
+			}
+		}(pc, pcm[idx])
+	}
+	wg.Wait()
+
+
+}
+
 func main() {
 	//Produce()
 	fmt.Println("=================")
-	Consume()
+	//Consume()
+	//Consumer2()
 }
---
+++ src/kakfa_consumer_group/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"log"
+	"os"
+	"github.com/wvanbergen/kafka/consumergroup"
+	"github.com/wvanbergen/kazoo-go"
+	"time"
+	"fmt"
+	"os/signal"
+)
+
+func init() {
+	sarama.Logger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
+}
+
+func main()  {
+	zkAddrStr := "localhost:2181"
+	gg := "h-test-x1"
+	topic := "test2"
+
+	config := consumergroup.NewConfig()
+	config.Offsets.Initial = sarama.OffsetNewest
+	config.Offsets.ProcessingTimeout = 10 * time.Second
+	var zookeeperNodes []string
+	zookeeperNodes, config.Zookeeper.Chroot = kazoo.ParseConnectionString(zkAddrStr)
+	fmt.Println(zookeeperNodes)
+	fmt.Println("xxx")
+	fmt.Println(config.Zookeeper.Chroot)
+
+
+	consumer, consumerErr := consumergroup.JoinConsumerGroup(gg, []string{topic,}, zookeeperNodes, config)
+	if consumerErr != nil {
+		fmt.Println("xxx", consumerErr)
+		log.Fatalln(consumerErr)
+	}
+
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		if err := consumer.Close(); err != nil {
+			sarama.Logger.Println("Error closing the consumer", err)
+		}
+	}()
+
+	go func() {
+		for err := range consumer.Errors() {
+			log.Println(err)
+		}
+	}()
+
+
+	eventCount := 0
+	offsets := make(map[string]map[int32]int64)
+
+	for message := range consumer.Messages() {
+		fmt.Println(message)
+		if offsets[message.Topic] == nil {
+			offsets[message.Topic] = make(map[int32]int64)
+		}
+
+		eventCount += 1
+		if offsets[message.Topic][message.Partition] != 0 && offsets[message.Topic][message.Partition] != message.Offset-1 {
+			log.Printf("Unexpected offset on %s:%d. Expected %d, found %d, diff %d.\n", message.Topic, message.Partition, offsets[message.Topic][message.Partition]+1, message.Offset, message.Offset-offsets[message.Topic][message.Partition]+1)
+		}
+
+		// Simulate processing time
+		time.Sleep(5 * time.Second)
+
+		offsets[message.Topic][message.Partition] = message.Offset
+		consumer.CommitUpto(message)
+	}
+
+	log.Printf("Processed %d events.", eventCount)
+	log.Printf("%+v", offsets)
+
+}
