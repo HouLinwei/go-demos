b1a88a76b40babfd2cc8f8fafe512fcca9533545
houlinwei <1507509064@qq.com>
2017-06-02 17:41:02 +0800

    sync

---
+++ src/use_logrus/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+func main()  {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Debug("A walrus appears")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Warn("A walrus appears")
+}
---
+++ src/use_sync/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+func main()  {
+	var once sync.Once
+	onceBody := func() {
+		fmt.Println("Only once")
+	}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onceBody)
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+	//fmt.Println()
+	//a := int32(1)
+	//atomic.AddInt32(&a, 1)
+	//fmt.Println(a)
+	//atomic.StoreInt32(&a, 100)
+	//fmt.Println(a)
+	//atomic.LoadInt32(&a)
+	//fmt.Println(a)
+}
