97d8af4a925e19bdc9f0d6685780704ba7233ac8
houlinwei <1507509064@qq.com>
2017-07-13 10:11:31 +0800

    copy

---
+++ src/copy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type P struct {
+	Name string
+	Age int
+	T  *T
+}
+
+type T struct {
+	Name string
+	Age int
+}
+
+func(p *P)DeepCopy() (*P){
+	//tmp := p
+	//obj := *tmp
+	obj := *p
+	return &obj
+}
+
+func main(){
+	T := &T{"xxx", 100}
+	p := P{"Houlinwei", 18, T}
+	p2 := p.DeepCopy()
+	p2.T.Age= 101
+	fmt.Println(p.T.Age, "==", p2.T.Age)
+}
