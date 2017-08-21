e54bf7fe89ac5fb9818e7c42ce7ebf8a50c8fdec
houlinwei <timeror@gmail.com>
2017-08-21 17:49:42 +0800

    add CPU profile demo

--- src/al_shuffer/main.go
+++ src/al_shuffer/main.go
@@ -8,8 +8,8 @@ import (
 
 func main()  {
 	//raw := []int{0,1,2,3,4,5,6,7,8,9,10}
-	shufflered := shuffler(10,19)
-	fmt.Println(shufflered)
+	res := shuffler(10,19)
+	fmt.Println(res)
 }
 
 
---
+++ src/profile_cpu/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"runtime/pprof"
+	"runtime"
+	"os/signal"
+)
+
+func main()  {
+
+	runtime.MemProfileRate = 1
+	fileName := "/tmp/cpuprofile.p"
+	f, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	go func() {
+		fmt.Println(" ===== test profile cpu ===== ")
+		i := 10
+		for{
+			i ++
+			fmt.Println(fab(i))
+			time.Sleep(time.Second*1)
+		}
+	}()
+
+
+	terminate := make(chan os.Signal)
+	signal.Notify(terminate, os.Interrupt)
+	<-terminate
+}
+
+func fab(x int) int{
+	if x == 1 || x == 2{
+		return x
+	}else{
+		return fab(x-1)+ fab(x-2)
+	}
+}
