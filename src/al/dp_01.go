b02192a129edbf71dd93f8292383ebfa2dab8de5
houlinwei <timeror@gmail.com>
2017-09-15 15:22:12 +0800

    update

--- src/al/dp_01.go
+++
@@ -1,5 +0,0 @@
-package al
-
-func DP(){
-
-}
--- src/al_tmp/main.go
+++ src/al_tmp/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"os/exec"
-	"fmt"
-)
+import "fmt"
 
 func main()  {
-	println("Justice.")
-	path, err := exec.LookPath("go")
-	if err != nil {
-		fmt.Println(err)
-	}else {
-		fmt.Println(path)
-	}
+	fmt.Println("X")
 }
